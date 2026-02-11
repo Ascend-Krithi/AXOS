685efe2b5c581bb665e61a655f1b61231437aa7a
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-11 11:53:51 +0530

    Deploying Page Objects

--- Scripts/TestScripts.py
+++ Scripts/TestScripts.py
@@ -1,4 +1,5 @@
-{Import necessary modules}
+# Import necessary modules
+from Pages.RuleConfigurationPage import RuleConfigurationPage
 
 class TestLoginFunctionality:
     def __init__(self, page):
@@ -12,4 +13,30 @@ class TestLoginFunctionality:
 
     async def test_remember_me_functionality(self):
         await self.login_page.navigate()
-        await self.login_page.fill_email('
+        await self.login_page.fill_email('')
+
+class TestRuleConfiguration:
+    def __init__(self, driver):
+        self.page = RuleConfigurationPage(driver)
+
+    def test_negative_amount_rule_creation_and_validation(self):
+        rule_id = 'NEG001'
+        rule_name = 'Negative Amount Rule'
+        self.page.create_rule_negative_amount(rule_id, rule_name)
+        self.page.verify_negative_amount_error()
+
+    def test_zero_amount_rule_creation_and_validation(self):
+        rule_id = 'ZERO001'
+        rule_name = 'Zero Amount Rule'
+        self.page.create_rule_zero_amount(rule_id, rule_name)
+        self.page.verify_zero_amount_error()
+
+    def test_large_amount_rule_creation_and_validation(self):
+        rule_id = 'LARGE001'
+        rule_name = 'Large Amount Rule'
+        expected_amount = 999999999.99
+        self.page.create_rule_large_amount(rule_id, rule_name)
+        self.page.verify_large_amount_success()
+        self.page.verify_amount_precision(expected_amount)
+        expected_balance = 1000000000.00
+        self.page.trigger_rule_and_verify_execution(expected_balance)
